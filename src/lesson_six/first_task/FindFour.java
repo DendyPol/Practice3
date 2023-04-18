d40dff5edd125696443ecb6324406152b0622a22
DendyPol <Polovinko_91@icloud.com>
2023-04-18 18:16:44 +0300

    homework_lesson_six_commit

--- src/lesson_six/first_task/FindFour.java
+++ src/lesson_six/first_task/FindFour.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FindFour {
-
   public List<Integer> returnArray(List<Integer> array) throws MyRuntimeException {
     var result = new ArrayList<Integer>();
     if (!array.contains(4)) {
